16ad640ec2feda1e37b0285efc7267227c47c322
Saied <elsaied.samaka1997@gmail.com>
2023-08-02 14:04:20 +0300

    setup users endpoint

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -7,9 +7,8 @@ const getUsers = async (req, res) => {
     try {
       const users = await User.findAll({
         where: condition,
-        include: { all: true },
       });
-      if (!users) return res.status(404).json({ message: "No users found" });
+      if (!users) return res.status(404).json({ message: "No users found. [users controller]" });
       res.status(200).json(users);
     } catch (err) {
       res.status(500).json({
@@ -20,32 +19,32 @@ const getUsers = async (req, res) => {
 // post a new user 
 const createUser = async (req,res)=>{
     try {
-        const user = await User.create({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            phonenumber: req.body.phonenumber,
-            countrycode: req.body.countrycode,
-            email: req.body.email,
-            password: req.body.password,
-            passwordConfirmation: req.body.passwordConfirmation,
-          });
+     const user = await User.create({
+         firstname: req.body.firstname,
+         lastname: req.body.lastname,
+         phonenumber: req.body.phonenumber,
+         countrycode: req.body.countrycode,
+         email: req.body.email,
+         birthdate: req.body.birthdate,
+         address: req.body.address,
+       });
     res.status(201).json(user);
     } catch (err) {
-        res.status(500).json({
-            message: err.message || "Some error occurred while creating user.",
-          }); 
+   res.status(500).json({
+       message: err.message || "Some error occurred while creating user. [users controller]",
+     }); 
     }
 }
 
 // Get User by ID
 const getUserById = async (req, res) => {
     try {
-      const user = await User.findByPk(req.params.id, { include: Role });
-      if (!user) return res.status(404).json({ message: "No user found [users controller] " });
+      const user = await User.findByPk(req.params.id);
+      if (!user) return res.status(404).json({ message: "No user found. [users controller] " });
       res.status(200).json(user);
     } catch (err) {
       res.status(500).json({
-        message: err.message || "Some error occurred while retrieving users.",
+        message: err.message || "Some error occurred while retrieving user.",
       });
     }
   };
@@ -61,20 +60,17 @@ const updateUserById = async (req, res) => {
         phonenumber: req.body.phonenumber || user.phonenumber,
         countrycode: req.body.countrycode || user.countrycode,
         email: req.body.email || user.email,
-        isActive: req.body.active,
-        role: user.setRole(role) || user.role,
-        password: req.body.password || user.password,
-        passwordConfirmation:
-          req.body.passwordConfirmation || user.passwordConfirmation,
+        birthdate: req.body.birthdate||user.birthdate,
+        address: req.body.address || user.address,
       });
       res.status(200).json(user);
       if (!user)
         return res
           .status(404)
-          .json({ message: "No user found [user controller]" });
+          .json({ message: "No user found. [user controller]" });
     } catch (err) {
       res.status(500).json({
-        message: err.message || "Some error occurred while retrieving users.",
+        message: err.message || "Some error occurred while updateing user.",
       });
     }
   };
@@ -83,12 +79,12 @@ const updateUserById = async (req, res) => {
 const deleteUserById = async (req, res) => {
     try {
       const user = await User.findByPk(req.params.id);
-      if (!user) return res.status(404).json({ message: "No user found [user controller]" });
+      if (!user) return res.status(404).json({ message: "No user found. [user controller]" });
       user.destroy();
       res.status(200).json(user);
     } catch (err) {
       res.status(500).json({
-        message: err.message || "Some error occurred while retrieving users.",
+        message: err.message || "Some error occurred while deleteing user.",
       });
     }
   };
