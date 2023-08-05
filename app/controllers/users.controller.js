9f81c9c538231f1d777cb44ac56e57b334c642cd
Saied <elsaied.samaka1997@gmail.com>
2023-08-05 12:27:26 +0300

    add pagination helper function

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,15 +1,20 @@
+const { getPagination, getPagingData } = require("../helper/getPagination");
+
 const User = require("../models").user;
 
 // Get List of Users
 const getUsers = async (req, res) => {
-    const { email } = req.query;
+    const { email , page, size} = req.query;
+    const { limit, offset } = getPagination(page, size);
     let condition = email ? { email: { [Op.like]: `%${email}%` } } : null;
     try {
-      const users = await User.findAll({
+      const users = await User.findAndCountAll({
         where: condition,
+        limit, offset
       });
       if (!users) return res.status(404).json({ message: "No users found. [users controller]" });
-      res.status(200).json(users);
+      const response = getPagingData(users, page, limit);
+      res.status(200).json(response);
     } catch (err) {
       res.status(500).json({
         message: err.message || "Some error occurred while retrieving users.",
